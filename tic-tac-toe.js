ef95a2d34b7accfb0c794dcf4a81d254c1f31e11
Brandon Ellington <brandymathic@gmail.com>
2024-10-24 15:08:38 -0500

    Edited tic-tac-toe.js (Exercise 1)

---
+++ tic-tac-toe.js
@@ -0,0 +1,9 @@
+//Exercise 1 - Layout the board
+document.addEventListener("DOMContentLoaded", () => {
+    const squares = document.querySelectorAll("#board div");
+    squares.forEach( square =>{
+        square.classList.add("square");
+        
+    });
+});
+
