bd2244726c6ac293719d2300be6e41780ad4b82c
Brandon Ellington <brandymathic@gmail.com>
2024-10-27 13:36:32 -0500

    Edited tic-tac-toe.js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -40,3 +40,26 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 });
 
+// Exercise 4 : Check the winner and update the status
+function checkWinner() {
+  const winningCombinations = [
+    [0, 1, 2], [3,4,5], [6,7,8],
+    [0,3,6], [1,4,7], [2,5,8],
+    [0,4,8], [2,4,6],
+  ];
+  for (let i = 0; i < winningCombinations.length; i++) {
+    const combination = winningCombinations[i];
+    if (
+      gameState[combination[0]] !== null &&
+      gameState[combination[0]] === gameState[combination[1]] &&
+      gameState[combination[0]] === gameState[combination[2]]
+    ) {
+      const status = document.getElementById("status");
+      status.textContent = `Congratulations! ${gameState[combination[0]]} is the Winner!`;
+      status.classList.add("you-won");
+      return;
+    }
+  }
+}
+
+
